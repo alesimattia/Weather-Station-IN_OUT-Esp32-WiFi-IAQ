a13f2e1d031ecbc7b9631ee294b913bb9feef74e
Alesi Mattia <alesimattia@gmail.com>
2020-11-19 17:13:21 +0100

    Unbundled result
    - Unbundled calculations & sensor readings from the calling function.
    - Global variables holds the data -> then a specific function will print to the TFT screen

--- Weather-station-wifi.ino
+++ Weather-station-wifi.ino
@@ -1,6 +1,6 @@
+#include <Adafruit_BMP280.h>
+
 #include <Adafruit_BME680.h>
-#include <bme680.h>
-#include <bme680_defs.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <RTClib.h>
@@ -9,42 +9,48 @@
 #define batt_in 32
 #define rx_pin 2
 
-
-static const char daysOfTheWeek[7][12] = {"Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"};
-
-float voltage;
-
-/*--------------- Oggetti sensori ---------------*/
+/*-------------------- Sensori ------------------*/
 RTC_DS3231 rtc;
 Adafruit_BME680 bme;
+Adafruit_BMP280 bmp;
 
+
+/*------------------ Variabili globali -------------------*/
+
+static const char daysOfTheWeek[7][12] = {"Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"};
+DateTime currentTime;
+float voltage;
+int vPercent;
+float voltage_ext;
+int vPercent_ext;
+float temp;
+float temp_ext;
+float humidity;
+float humidity_ext;
+float pressure;
+float pressure_ext;
+float airIndex;
 
 
 void setup() {
 
   Serial.begin(115200);
   Wire.begin(); //I2C start
-  adcAttachPin(batt_in);
-  pinMode(batt_in, INPUT);
   
-  /*--------------- Inizializzazione sensori ---------------*/
-  if (! rtc.begin() ) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  if (! bme.begin() ) {
-    Serial.println("Couldn't find BME");
-    //while(1);
-  }
+  /*------------------- Inizializzazione sensori ---------------------*/
+  if (! rtc.begin() )   Serial.println("Couldn't find RTC");
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
-  /*----------------------------------------------------*/
-
+  if (! bmp.begin(0x76) )   Serial.println("Couldn't find BMP");
+  
+  adcAttachPin(batt_in);
+  adcStart(batt_in);
+  pinMode(batt_in, INPUT);
+  /*------------------------------------------------------------------*/
 }
 
 
 void readBattery() {
-  adcStart(batt_in);
   const byte nReadings = 64;
   float voltage_reading = 0;
   float voltage = 0;
@@ -54,44 +60,55 @@ void readBattery() {
   }
   /** 5.1 è il voltaggio max da leggere --> resistori tarati su quel livello
       r1 (to device) 1Mohm --- r2 (to gnd) 1Mohm
-      Nel caso di resistori diversi moltiplicare per (r1/r2)
-      In questo caso il fattore di conversione è 1/2
+      Nel caso di resistori diversi moltiplicare per  r1/(r1+r2)
+      In questo caso il fattore di conversione è 1M/(1M+1M)
   */
   voltage = (voltage_reading / 64) * 3.3 / 4095 / 2;
-  Serial.print("Voltage: " + (String) voltage);
-  if(voltage > 0){
-    Serial.println("\t" + (String) map(voltage, 3.2, 4.2, 0, 100)  + "%");
-  }
-  else Serial.println("\t 0%");
+  vPercent = map(voltage, 3.2, 4.21, 0, 100);
 }
 
 
 void getTime() {
-  DateTime now = rtc.now();
-
-  Serial.print((String)now.day() + "/" + (String)now.month() + "/" + (String)now.year() +
-               "  " + (String)daysOfTheWeek[now.dayOfTheWeek()] + "  " + (String)now.hour() +
-               ":" + (String) now.minute() + ":" + (String)now.second() );
+  currentTime = rtc.now();
+  
+  Serial.print((String)currentTime.day() + "/" + (String)currentTime.month() + "/" + (String)currentTime.year() +
+               "  " + (String)daysOfTheWeek[currentTime.dayOfTheWeek()] + "  " + (String)currentTime.hour() +
+               ":" + (String) currentTime.minute() + ":" + (String)currentTime.second() );
   //Misura circa 1°C in eccesso
   Serial.println("  RTC-Temp: " + (String) (rtc.getTemperature() - 1) );
 }
 
 
 void ambientMeasurement() {
+  temp = bmp.readTemperature();
+  pressure = bmp.readPressure()/100;
+}
+
+
+void printToSerial(){
+  
+  /*--------------------------------------------------------------------------------*/
+  Serial.print("Voltage: " + (String)voltage +"V");
+  if(voltage > 0){
+    Serial.println("\t" + (String)vPercent + "%");
+  }
+  else Serial.println("\t 0% \n");
+  /*--------------------------------------------------------------------------------*/
+  Serial.println("Temperature: " + (String) temp + " °C");
+  Serial.println("Pressure: " + (String) pressure + " hPa");
+  Serial.println();
+  /*--------------------------------------------------------------------------------*/
   Serial.println("----------------------------------------");
-  //bme.getPressureSensor()->printSensorDetails();
-  Serial.println("Temperature: " + (String) bme.readTemperature() + " °C");
-  Serial.println("Pressure: " + (String) bme.readPressure() + " Pa");
-  Serial.println("Humidity: " + (String) bme.readHumidity() + " %RH");
+  
 }
 
 
 void loop() {
-  Serial.println("----------------------------------------");
+  
   readBattery();
   getTime();
-  //ambientMeasurement();
-
-  Serial.println();
-  delay(2000);
+  ambientMeasurement();
+  
+  printToSerial(); 
+  delay(4000);
 }
