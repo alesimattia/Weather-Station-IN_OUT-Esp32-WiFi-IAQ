698a5676e9a7ce7a5ebbd80e6cb23294dc7b6e32
Alesi Mattia <alesimattia@gmail.com>
2021-01-02 23:05:00 +0100

    Wired the display
    - Wired the 4.0" ST7796 TFT screen
    - Implemented screen code
    Striving with the GFX library to display an image instead of a drawing

--- ext_sensor/ext_sensor.ino
+++ ext_sensor/ext_sensor.ino
@@ -1,14 +1,18 @@
+#include <bsec.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
 
+#define esp8266
+#define ARDUINO_ARCH_ESP32
+
 /** Weather-Station (receiver) must be aware 
  * of these for synchronization */
 static const byte TIME_TO_SLEEP = 4200;
-static const byte TIME_TO_LISTEN_HTTP = 1;     
-static const String batt_in = "A0";
+static const byte TIME_TO_LISTEN_HTTP = 1;
 
 
 const char* ssid = "ESP-sensor";
@@ -16,6 +20,7 @@ const char* password = "esp8266sensor";
 AsyncWebServer server(80);
 
 Adafruit_BME680 bme;
+Bsec util;
 
 float voltage_ext;
 int vPercent_ext;
@@ -31,21 +36,23 @@ void setup() {
     Serial.begin(115200);   //Will be removed for 'production' to save battery
     Wire.begin(); //I2C start
     system_deep_sleep_set_option(0);
-
-    pinMode(batt_in, INPUT);
+    pinMode('A0', INPUT);
 
 
     /*----------------------------- BME680 Sensor ------------------------------*/
 
-    if (! bme.begin(0x76))  Serial.println("Couldn't find bme, but keep working");
+    if (! bme.begin(0x77))  Serial.println("Couldn't find BME sensor, but keep working");
     
     /** Weather/Climate-monitor  Calibration */
-    bme.setSampling(Adafruit_bme280::MODE_FORCED, 
-                    Adafruit_bme280::SAMPLING_X1, // temperature 
-                    Adafruit_bme280::SAMPLING_X1, // pressure
-                    Adafruit_bme280::FILTER_OFF ); 
+    bme.setTemperatureOversampling(BME680_OS_1X);
+    bme.setHumidityOversampling(BME680_OS_1X);
+    bme.setPressureOversampling(BME680_OS_1X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_0);
+    bme.setGasHeater(320, 150); 
 
-
+    util.begin(BME680_I2C_ADDR_SECONDARY, Wire);
+    checkutilStatus();
+    
     /*-------------------------- WIFI async web server ----------------------*/
     WiFi.softAP(ssid, password);
     //Serial.println( WiFi.softAPIP() );  /** 192.168.4.1 */
@@ -57,22 +64,17 @@ void setup() {
         request->send_P(200, "text/plain",  temp );
     } );
     server.on("/temp", HTTP_GET, [](AsyncWebServerRequest *request){
-        String ris = String( bme.readTemperature() - 1.5 );  //ESP self heathing
-        request->send_P(200, "text/plain", ris.c_str() );
+        request->send_P(200, "text/plain", String( bme.readTemperature() ).c_str() );
     } );
     server.on("/press", HTTP_GET, [](AsyncWebServerRequest *request){
-        String ris = String( bme.readPressure() / 100 );  //hPA
-        request->send_P(200, "text/plain", ris.c_str() );
+        request->send_P(200, "text/plain", String( bme.readPressure() / 100.0 ).c_str() );
     } );
-    
-    /*server.on("/hum", HTTP_GET, [](AsyncWebServerRequest *request){
-        char ris = (char)  bme.readHumidity();
-        request->send_P(200, "text/plain", &ris );
+    server.on("/hum", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", String( bme.readHumidity() ).c_str() );
     } );
     server.on("/air", HTTP_GET, [](AsyncWebServerRequest *request){
-        char ris = (char)  bme.readGas();
-        request->send_P(200, "text/plain", &ris );
-    } );*/
+        request->send_P(200, "text/plain", String( bme.readGas() / 1000.0 ).c_str() );
+    } );
 
     server.begin();
 }
@@ -80,10 +82,19 @@ void setup() {
 
 //Only for testing (serial output) --> will be removed in 'production'
 void ambientMeasurement() {
-    temp_ext = ( bme.readTemperature() - 1.5);  //BME self heating
-    pressure_ext = bme.readPressure() / 100;
-    //humidity_ext = bme.readHumidity();
-    //airIndex = bme.readGas();
+    bme.performReading();
+    temp_ext = bme.temperature;
+    pressure_ext = bme.pressure / 100.0;
+    humidity_ext = bme.humidity;
+    airIndex = bme.gas_resistance / 1000.0;
+
+    while (! util.run()) { // If no data is available
+        Serial.println("BSEC calculations not ready");
+        //return;
+    }
+    Serial.println("\nIAQ: " + String(util.iaq) + "\nAccuracy: " + String(util.iaqAccuracy) 
+                 + "\nStatic-IAQ: " + String(util.staticIaq) +"\nCO2 " + String(util.co2Equivalent)
+                 + "\nBreath-VOC" + String(util.breathVocEquivalent) );
 }
 
 
@@ -95,50 +106,63 @@ void ambientMeasurement() {
  * r2/(r1+r2) so multiply what red per 1/attenuation 
  **/
 float readBattery() {
-
+    //My charging circuit: (Max-4.2V)(Min-3.2)
     const byte nReadings = 64;
     float voltage_reading = 0;
 
     for (byte x = 0; x < nReadings; x++)
-        voltage_reading += analogRead(batt_in);
+        voltage_reading += analogRead('A0');
 
-    /** Wemos D1 Mini has an internal voltage divider to measure up 
-     * to 3.3V;  Bare ESP board can measure up to 1V
-     * My board is actually 'damaged' so I have to applly a sperimental offset
-    **/
-    const float offset = 3.42;
-    voltage_ext = (voltage_reading / nReadings) * 3.3 / 1024 * offset;
-    return voltage_ext;
-    //My charging circuit: (Max-4.2V)(Min-3.2)
+    //res_to_batt = 1.05 MOhm
+    //res_in_A0 = 315.75 kOhm
+    return voltage_ext = (voltage_reading / nReadings) * 3.3 / 1024 ;;
 }
 
 
 void printToSerial() {
     /*--------------------------------------------------------------------------------*/
-    Serial.print("\nVoltage: ");
+    Serial.print("Voltage: ");
     Serial.print(voltage_ext, 4);
     Serial.print("V");
     Serial.println("\t" + (String) vPercent_ext + "%");
     /*--------------------------------------------------------------------------------*/
     Serial.println("Temperature: " + (String) temp_ext + " °C");
     Serial.println("Pressure: " + (String) pressure_ext + " hPa");
-    //Serial.println("Humidity: " + (String) humidity_ext + " %RH");
-    //Serial.println("Air Index: " + (String) airIndex + " tVOC");
-    Serial.println();
+    Serial.println("Humidity: " + (String) humidity_ext + " %RH");
+    Serial.println("Air Index: " + (String) airIndex + " KOhms");
+    Serial.println("");
     Serial.println("----------------------------------------");
     /*--------------------------------------------------------------------------------*/
 }
 
+void checkutilStatus(void)
+{
+  if (util.status != BSEC_OK) {
+    if (util.status < BSEC_OK) {
+      Serial.println( "BSEC error code : " + String(util.status));
+    } else {
+      Serial.println( "BSEC warning code : " + String(util.status));
+    }
+  }
+ 
+  if (util.bme680Status != BME680_OK) {
+    if (util.bme680Status < BME680_OK) {
+      Serial.println( "BME680 error code : " + String(util.bme680Status));
+    } else {
+      Serial.println( "BME680 warning code : " + String(util.bme680Status));
+    }
+  }
+}
 
 /** A delay will let the ESP to stay on for the time 
      * to being reacheable to the weather-station via WIFI 
      * and get the data from ext_sensor device --> then sleep
-    **/ 
+**/ 
 void loop() {
     readBattery();
     ambientMeasurement();
     printToSerial();
-
-    delay(TIME_TO_LISTEN_HTTP * 1000);
-    ESP.deepSleep(TIME_TO_SLEEP * 1E6, WAKE_RF_DEFAULT);
+    delay(3000);
+    //delay(TIME_TO_LISTEN_HTTP * 1000);
+    //ESP.deepSleep(TIME_TO_SLEEP * 1E6, WAKE_RF_DEFAULT);
 }
